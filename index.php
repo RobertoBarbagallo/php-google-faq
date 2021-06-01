c313cee35b7121bb47534a10b0e765f4b503380b
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-06-01 17:53:16 +0200

    header completato

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Google Faq</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="flex-cont">
+                <div class="img-cont">
+                    <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="google logo">
+                </div>
+                <div class="flex-g1 header-text align-fe">Privacy e termini</div>
+                <i class="fas fa-bars flex-cont"></i>
+                <div class="round flex-cont">R</div>
+            </div>
+            <ul class="flex-cont">
+                <li>
+                    <a href="">Introduzione</a>
+                </li>
+                <li>
+                    <a href="">Norme sulla Privacy</a>
+                </li>
+                <li>
+                    <a href="">Termini di servizio</a>
+                </li>
+                <li>
+                    <a href="">Teconologie</a>
+                </li>
+                <li>
+                    <a class="current-link" href="">Domande frequenti</a>
+                </li>
+            </ul>
+            <hr>
+        </div>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+</body>
+
+</html>
